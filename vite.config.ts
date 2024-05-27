2f837a652e4fb83b413ff7bc08d08e490b824609
DomilsonFirmino <almeidagilbertofirmino@gmail.com>
2024-05-27 02:55:50 +0100

    PUT: RepoLink

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Countries_API",
+  base: "https://domilsonfirmino.github.io/Countries_API/",
 })
